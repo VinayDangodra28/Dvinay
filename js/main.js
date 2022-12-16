4e038b872023296989451f6875b6219b9ecc9a59
dvinay <vinaydangodra28@gmail.com>
2022-12-16 22:15:48 +0530

    regular commit

---
+++ js/main.js
@@ -0,0 +1,30 @@
+// navbar
+function myFunction() {
+    document.getElementById("nav-h").classList.toggle("open");
+    nav-h.classList.remove('close');
+  }
+  
+  // Closing the dropdown if the user clicks outside of it
+  window.onclick = function(e) {
+    if (!e.target.matches('.dropbtn')) {
+    var sider  = document.getElementById("nav-h");
+      if (nav-h.classList.contains('open')) {
+        nav-h.classList.remove('open');
+      }
+    }
+  }
+  // smooth scroll
+  var navMenuAnchorTags = document.querySelectorAll('.nav-social-h a');
+  console.log(navMenuAnchorTags);
+
+// preloader
+var loader = document.getElementById("preloader");
+setTimeout(timeout, 7000);
+function timeout() {
+  endAnimation();
+}
+function endAnimation() {
+    loader.setAttribute("class", "afterloader");
+}
+
+
